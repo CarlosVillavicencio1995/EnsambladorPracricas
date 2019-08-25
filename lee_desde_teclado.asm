cf068779ef364e173f7175b1f6212a2f028b6ba7
CarlosVillavicencio1995 <41699989+CarlosVillavicencio1995@users.noreply.github.com>
2019-08-25 16:20:26 -0600

    Update lee_desde_teclado.asm

--- lee_desde_teclado.asm
+++ lee_desde_teclado.asm
@@ -1,3 +1,18 @@
+%macro presentar 2
+mov eax, 4
+mov ebx, 1
+mov ecx, %1
+mov edx, %2
+int 80h
+%endmacro
+
+%macro ing_teclado 2
+mov eax, 3
+mov ebx, 2
+mov ecx, %1
+mov edx, %2
+int 80h
+%endmacro
 section  .data 
 	Msg db 'ingrese un numero: ' 
 	lenMsg equ $-Msg  ;longitud de userMsg
@@ -10,31 +25,14 @@ section .bss  ;dato en dondee almacenara los numero a ingresar
 section .text  ;Code Segment
 	global _start
 _start:
+	presentar Msg, lenMsg 
+	
+	ing_teclado num, 5
+		
+	presentar Msg1, lenMsg1
 
-	mov eax, 4
-	mov ebx, 1
-	mov ecx, Msg   
-	mov edx, lenMsg 
-	int 80h    
-
-	mov eax, 3
-	mov ebx, 2
-	mov ecx, num 
-	mov edx, 5   
-	int 80h
-
-	mov eax, 4
-	mov ebx, 1
-	mov ecx, Msg1
-	mov edx, lenMsg1
-	int 80h 
-
-	mov eax, 4
-	mov ebx, 1
-	mov ecx, num
-	mov edx, 5
-	int 80h 
-
+	presentar num, 5
+	
 	mov eax, 1
 	mov ebx, 0
 	int 80h
